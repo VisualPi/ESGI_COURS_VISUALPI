e3c3a0f74d32a0a2a743dcf9a377a6c010e1d8c0
VisualPi <gintakun2@yahoo.fr>
2015-10-19 02:18:53 +0200

    Commit des projets de tests unreal

---
+++ UNREAL_ENGINE_PROJECTS/Tests/MyBlankProj/Source/MyBlankProj/MyBlankProjGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyBlankProj.h"
+#include "MyBlankProjGameMode.h"
+
+
+
+
---
+++ UNREAL_ENGINE_PROJECTS/Tests/MyBlankProj/Source/MyBlankProj/MyBlankProjGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MyBlankProjGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYBLANKPROJ_API AMyBlankProjGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
